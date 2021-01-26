6f14bbfcb65771221455bb317a92af63e113b563
yuri-song <sy120003@gmail.com>
2021-01-26 17:40:56 +0900

    1.26 재배포

--- app.php
+++ app.php
@@ -1,38 +1,18 @@
 <?php
-
-function getArticleTagsHtml($id) {
-    $article = &getArticleById($id);
-
-    $html = "";
-
-    foreach ( $article['tags'] as $tag ) {
-        $link = getArticleListByTagLink($tag);
-        $html .= " <a class=\"tag-link\" href=\"{$link}\">#" . $tag . "</a>";
-    }
-
-    return $html;
-}
-
 function getArticleLink($id) {
     return "article_detail.ssghtml.php?id={$id}&ext=html";
 }
-
 function getArticleListByTagLink($tag) {
     return "article_list_by_tag.ssghtml.php?tag={$tag}&ext=html";
 }
-
 function getFilesByEndsWith($endsWith) {
-
     $fileNames = [];
-
     $it = new RecursiveDirectoryIterator(".");
-
     foreach(new RecursiveIteratorIterator($it) as $file) {
         if ( endsWith($file->getFilename(), $endsWith) ) {
             $fileNames[] = $file->getFilename();
         }
     }
-
     return $fileNames;
 }
 
@@ -51,7 +31,7 @@ function getFiles() {
             continue;
         }
 
-        if ( strpos($file->getRealPath(), '\docs') !== false ) {
+        if ( strpos($file->getRealPath(), '\dist') !== false ) {
             continue;
         }
 
@@ -67,48 +47,15 @@ function getMaxArticleId() {
     if ( $maxId !== null ) {
         return $maxId;
     }
-
     $keys = array_keys($GLOBALS);
-
     $maxId = 0;
-
     foreach ( $keys as $key ) {
         if ( startsWith($key, "article") ) {
             $no = intval(str_replace("article", "", $key));
-
             if ( $no > $maxId ) {
                 $maxId = $no;
             }
         }
     }
-
     return $maxId;
-}
-
-function &getArticles() {
-    global $_allArticles;
-
-    return $_allArticles;
-}
-
-function &getArticleById($id) {
-    $articles = &getArticles();
-
-    return $articles[$id];
-}
-
-function &getArticlesByTag($tag) {
-    global $_allArticlesByTag;
-
-    if ( isset($_allArticlesByTag[$tag]) == false ) {
-        return [];
-    }
-
-    return $_allArticlesByTag[$tag];
-}
-
-function &getTags() {
-    global $_tags;
-
-    return $_tags;
 }
--- util.php
+++ util.php
@@ -3,7 +3,6 @@ function startsWith($haystack, $needle) {
     // search backwards starting from haystack length characters from the end
     return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
 }
-
 function endsWith($haystack, $needle) {
     // search forward starting from end minus needle length characters
     return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
@@ -12,7 +11,7 @@ function endsWith($haystack, $needle) {
 function deleteDirectory($dir) {
     $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
     $files = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::CHILD_FIRST);
-    
+
     foreach($files as $file) {
         if ($file->isDir()){
             rmdir($file->getRealPath());
@@ -22,4 +21,4 @@ function deleteDirectory($dir) {
     }
 
     @rmdir($dir);
-}
+} 
