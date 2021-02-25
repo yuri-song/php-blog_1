e298a07ea261b833c3f5f89c327bd979fb6222c6
yurong51 <76942943+yurong51@users.noreply.github.com>
2021-02-26 00:43:37 +0900

    .

--- pf.ssghtml.php
+++ pf.ssghtml.php
@@ -19,9 +19,16 @@ require_once "head.php";
 </section>
 
 <div class="section">
-  <a href="pf.detail.ssghtml.php">모바일 기획 - 1 [패션 어플리케이션 MW]</a>
+  <a href="pf/mobile/app-mw.ssghtml.php">모바일 기획 - 1 [패션 어플리케이션 MW]</a>
   <div class="img-box">
   <a href="pf/mobile/app-mw.ssghtml.php"><img src="https://yuri-song.github.io/logo_image/new/section1.png" alt=""></a>
+  </div>
+</div>
+
+<div class="section">
+  <a href="pf/mobile/web-odor.ssghtml.php">웹 리디자인 - 1 [odor]</a>
+  <div class="img-box">
+  <a href="pf/mobile/web-odor.ssghtml.php"><img src="https://yuri-song.github.io/logo_image/re/s.png" alt=""></a>
   </div>
 </div>
 <?php
---
+++ pf/mobile/web-odor.ssghtml.php
@@ -0,0 +1,9 @@
+<?php
+require_once "data.pf.odor.php";
+?>
+
+<link rel="stylesheet" href="css/pf.odor.css">
+<script src="js/pf.js" defer></script>
+
+<script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
