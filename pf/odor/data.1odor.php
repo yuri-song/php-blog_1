c5da313300ab8b5256ef293f45162d467e202097
yurong51 <76942943+yurong51@users.noreply.github.com>
2021-02-26 03:42:14 +0900

    .

--- pf/odor/data.1odor.php
+++ pf/odor/data.1odor.php
@@ -1,80 +1,79 @@
 <section class="con">
-  <div class="page1">
-    <span>about<br>
-      song yuri</span> 
+<div class="page1">
+  <span>about<br>
+    song yuri</span> 
 
 
-    <img src="https://yuri-song.github.io/logo_image/pf/page1.png" alt="">
-  </div>
-  <div class="page2">
-    <img src="https://yuri-song.github.io/logo_image/pf/page2.png" alt="">
-  </div>
-  <div class="page3">
-    <img src="https://yuri-song.github.io/logo_image/pf/page3.png" alt="">
-  </div>
-  <div class="page4">
-    <img src="https://yuri-song.github.io/logo_image/pf/page4.png" alt="">
-    <img src="https://yuri-song.github.io/logo_image/pf/page4-1.png" alt="">
-    <img src="https://yuri-song.github.io/logo_image/pf/page4-2.png" alt="">
-  </div>
-  <div class="page5">
-    <img src="https://yuri-song.github.io/logo_image/pf/page5.png" alt="">  </div>
-  <div class="page6">
-    <img src="https://yuri-song.github.io/logo_image/pf/page6.png" alt="">
-    <img src="https://yuri-song.github.io/logo_image/pf/page6-1.png" alt="">
-    <div></div>
-    <p class="typing-txt"> 안녕하세요 송유리입니다.</p> 
-  <p class="typing"></p> 
-  </div>
-  <div class="page7">
-    
-    <img src="https://yuri-song.github.io/logo_image/pf/page7.png" alt="">
-    <span>
-      일상을 기록하고 소통하는 것을 좋아합니다.
-      <br>
-일상을 기록하는 매체로 다양한 sns를 활용하여 주기적으로 업로드합니다.
-    </span>
-    <img src="https://yuri-song.github.io/logo_image/pf/page7-1.png" alt="">
-    <a href="https://www.instagram.com/yrong__chu51/" target="_blank">
-      <img src="https://yuri-song.github.io/logo_image/pf/0.png" alt="">
-    </a>
+  <img src="https://yuri-song.github.io/logo_image/pf/page1.png" alt="">
+</div>
+<div class="page2">
+  <img src="https://yuri-song.github.io/logo_image/pf/page2.png" alt="">
+</div>
+<div class="page3">
+  <img src="https://yuri-song.github.io/logo_image/pf/page3.png" alt="">
+</div>
+<div class="page4">
+  <img src="https://yuri-song.github.io/logo_image/pf/page4.png" alt="">
+  <img src="https://yuri-song.github.io/logo_image/pf/page4-1.png" alt="">
+  <img src="https://yuri-song.github.io/logo_image/pf/page4-2.png" alt="">
+</div>
+<div class="page5">
+  <img src="https://yuri-song.github.io/logo_image/pf/page5.png" alt="">  </div>
+<div class="page6">
+  <img src="https://yuri-song.github.io/logo_image/pf/page6.png" alt="">
+  <img src="https://yuri-song.github.io/logo_image/pf/page6-1.png" alt="">
+  <div></div>
+  <p class="typing-txt"> 안녕하세요 송유리입니다.</p> 
+<p class="typing"></p> 
+</div>
+<div class="page7">
+  
+  <img src="https://yuri-song.github.io/logo_image/pf/page7.png" alt="">
+  <span>
+  ‘일상을 기록하고 소통하는 것을 좋아합니다.’
 
-    <a href="https://www.instagram.com/gray_song05/" target="_blank">
-      <img src="https://yuri-song.github.io/logo_image/pf/0.png" alt="">
-    </a>
-    <a href="https://blog.naver.com/sy12003" target="_blank">
-      <img src="https://yuri-song.github.io/logo_image/pf/1.png" alt="">
-    </a>
-    <a href="https://www.youtube.com/channel/UCWX-EUKjncJYYoK65JsdiTg/playlists" target="_blank">
-      <img src="https://yuri-song.github.io/logo_image/pf/2.png" alt="">
-    </a>
+  </span>
+  <img src="https://yuri-song.github.io/logo_image/pf/page7-1.png" alt="">
+  <a href="https://www.instagram.com/yrong__chu51/" target="_blank">
+    <img src="https://yuri-song.github.io/logo_image/pf/0.png" alt="">
+  </a>
 
-  </div>
-  <div class="page8">
-    <img src="https://yuri-song.github.io/logo_image/pf/page8.png" >
-  </div>
-  <div class="page9">
-    <img src="https://yuri-song.github.io/logo_image/pf/page9.png" alt="">
-    <img src="https://yuri-song.github.io/logo_image/pf/page9-1.png" alt="">
-  </div>
-  <div class="page10">
-    <img src="https://yuri-song.github.io/logo_image/pf/page10.png" alt="">
-    <img src="https://yuri-song.github.io/logo_image/pf/page10-1.png" alt="">
-  </div>
-  <div class="page11">
-    <img src="https://yuri-song.github.io/logo_image/pf/page11.png" alt="">
-    <img src="https://yuri-song.github.io/logo_image/pf/page11-1.png" alt="">
-  </div>
-  <div class="page12">
-    <img src="https://yuri-song.github.io/logo_image/pf/page12.png" alt="">
-    <img src="https://yuri-song.github.io/logo_image/pf/page12-1.png" alt="">
-  </div>
-  <div class="page13">
-    <img src="https://yuri-song.github.io/logo_image/pf/page13.png" alt="">
-    <img src="https://yuri-song.github.io/logo_image/pf/page13-1.png" alt="">
-  </div>
-  <div class="page14">
-    <img src="https://yuri-song.github.io/logo_image/pf/page14.png" alt="">
-    <img src="https://yuri-song.github.io/logo_image/pf/page14-1.png" alt="">
-  </div>
+  <a href="https://www.instagram.com/gray_song05/" target="_blank">
+    <img src="https://yuri-song.github.io/logo_image/pf/0.png" alt="">
+  </a>
+  <a href="https://blog.naver.com/sy12003" target="_blank">
+    <img src="https://yuri-song.github.io/logo_image/pf/1.png" alt="">
+  </a>
+  <a href="https://www.youtube.com/channel/UCWX-EUKjncJYYoK65JsdiTg/playlists" target="_blank">
+    <img src="https://yuri-song.github.io/logo_image/pf/2.png" alt="">
+  </a>
+
+</div>
+<div class="page8">
+  <img src="https://yuri-song.github.io/logo_image/pf/page8.png" >
+</div>
+<div class="page9">
+  <img src="https://yuri-song.github.io/logo_image/pf/page9.png" alt="">
+  <img src="https://yuri-song.github.io/logo_image/pf/page9-1.png" alt="">
+</div>
+<div class="page10">
+  <img src="https://yuri-song.github.io/logo_image/pf/page10.png" alt="">
+  <img src="https://yuri-song.github.io/logo_image/pf/page10-1.png" alt="">
+</div>
+<div class="page11">
+  <img src="https://yuri-song.github.io/logo_image/pf/page11.png" alt="">
+  <img src="https://yuri-song.github.io/logo_image/pf/page11-1.png" alt="">
+</div>
+<div class="page12">
+  <img src="https://yuri-song.github.io/logo_image/pf/page12.png" alt="">
+  <img src="https://yuri-song.github.io/logo_image/pf/page12-1.png" alt="">
+</div>
+<div class="page13">
+  <img src="https://yuri-song.github.io/logo_image/pf/page13.png" alt="">
+  <img src="https://yuri-song.github.io/logo_image/pf/page13-1.png" alt="">
+</div>
+<div class="page14">
+  <img src="https://yuri-song.github.io/logo_image/pf/page14.png" alt="">
+  <img src="https://yuri-song.github.io/logo_image/pf/page14-1.png" alt="">
+</div>
 </section>
