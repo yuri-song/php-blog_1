033859e9cd92f7c19350387504e9c50433b61c46
yuri-song <sy120003@gmail.com>
2021-03-08 16:16:48 +0900

    .

--- pf.ssghtml.php
+++ pf.ssghtml.php
@@ -18,18 +18,32 @@ require_once "head.php";
   </h1>
 </section>
 
-<div class="section">
-  <a href="pf/mobile/app-mw.ssghtml.php">모바일 기획 - 1 [패션 어플리케이션 MW]</a>
-  <div class="img-box">
-  <a href="pf/mobile/app-mw.ssghtml.php"><img src="https://yuri-song.github.io/logo_image/new/section1.png" alt=""></a>
+<div class="con"><div class="img">
+  <div class="section section-1">
+    <div class="img-box">
+      <a href="pf/mobile/app-mw.ssghtml.php">
+        <img src="https://yuri-song.github.io/logo_image/new/section1.png" alt=""></a>
+      <div>
+        <span class="name">패션애플리케이션 'WM'</span>
+        <div class="bar"></div>
+        <a href="pf/mobile/app-mw.ssghtml.php"><button>프로세스 페이지</button></a>
+      </div>
+    </div>
   </div>
-</div>
 
-<div class="section">
-  <a href="pf/web/odor/odor1.ssghtml.php">웹 리디자인 - 1 [odor]</a>
-  <div class="img-box">
-  <a href="pf/web/odor/odor1.ssghtml.php"><img src="https://yuri-song.github.io/logo_image/re/s.png" alt=""></a>
+  <div class="section section-2">
+    <div class="img-box">
+      <a href="pf/web/odor/odor1.ssghtml.php"><img src="https://yuri-song.github.io/logo_image/pro/333.png" alt=""></a>
+      <div>
+        <span class="name">오도어 리뉴얼 디자인</span>
+        <div class="bar"></div>
+        <a href="pf/web/odor/odor1.ssghtml.php"><button>메인페이지</button></a>
+        <a href="pf/web/odor/odor.ssghtml.php"><button>프로세스 페이지</button></a>
+      </div>
+    </div>
   </div>
+
+</div>
 </div>
 
 
---
+++ pf/web/odor/odor.ssghtml.php
@@ -0,0 +1,15 @@
+<?php
+require_once "data.odor.php";
+?>
+
+<link rel="stylesheet" href="odor.css">
+<script src="odor.js" defer></script>
+
+<!-- 제이쿼리 불러오기 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- gsap 불러오기 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/ScrollTrigger.min.js"></script>
+
+
