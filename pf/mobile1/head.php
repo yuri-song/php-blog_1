df4b79a9c074a008a3759826a186943d49544d01
yurong51 <76942943+yurong51@users.noreply.github.com>
2021-03-10 23:52:21 +0900

    .

--- pf/mobile1/head.php
+++ pf/mobile1/head.php
@@ -10,7 +10,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <!-- 테일윈드 불러오기 -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.3/tailwind.min.css">
-
+    <!-- gsap 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js"></script>
+    
     <link rel="stylesheet" href="index.css">
     <script scr="index.js" defer></script>
 
