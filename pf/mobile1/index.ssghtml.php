1d4fa4d135116cfc63ab0a0f3a4115fbfadab852
yurong51 <sy120003@gmail.com>
2021-03-17 01:56:28 +0900

    .

--- pf/mobile1/index.ssghtml.php
+++ pf/mobile1/index.ssghtml.php
@@ -6,35 +6,35 @@ require_once "head.php";
 <div class="main">
     <div class="con">
         <div class="main_seaction1">
-            <img src="https://yuri-song.github.io/logo_image/mobile1/1.png" alt="">
+            <img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/1.png" alt="">
         </div>
-        <div class="main_seaction2"><img src="https://yuri-song.github.io/logo_image/mobile1/2.png" alt="">
-            <img src="https://yuri-song.github.io/logo_image/mobile1/2-1.png" alt=""></div>
+        <div class="main_seaction2"><img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/2.png" alt="">
+            <img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/2-1.png" alt=""></div>
         <div class="main_seaction3">
-            <img src="https://yuri-song.github.io/logo_image/mobile1/3.png" alt="">
-            <img src="https://yuri-song.github.io/logo_image/mobile1/4-1.png" alt="">
-            <img src="https://yuri-song.github.io/logo_image/mobile1/4-2.png" alt="">
-            <img src="https://yuri-song.github.io/logo_image/mobile1/4-3.png" alt="">
+            <img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/3.png" alt="">
+            <img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/4-1.png" alt="">
+            <img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/4-2.png" alt="">
+            <img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/4-3.png" alt="">
         </div>
-        <div class="main_seaction4"><img src="https://yuri-song.github.io/logo_image/mobile1/5.png" alt="">
-            <img src="https://yuri-song.github.io/logo_image/mobile1/5-1.png" alt="">
+        <div class="main_seaction4"><img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/5.png" alt="">
+            <img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/5-1.png" alt="">
         </div>
-        <div class="main_seaction5"><img src="https://yuri-song.github.io/logo_image/mobile1/6.png" alt=""></div>
+        <div class="main_seaction5"><img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/6.png" alt=""></div>
         <div class="main_seaction6">
-            <img src="https://yuri-song.github.io/logo_image/mobile1/7.png" alt="">
+            <img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/7.png" alt="">
         </div>
         <div class="main_seaction7">
-            <img src="https://yuri-song.github.io/logo_image/mobile1/8.png" alt="">
-            <img src="https://yuri-song.github.io/logo_image/mobile1/9.png" alt="">
+            <img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/8.png" alt="">
+            <img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/9.png" alt="">
         </div>
         <div class="main_seaction8">
             <img src="https://github.com/yuri-song/logo_image/blob/master/mobile1/10.png?raw=true" alt="">
         </div>
         <div class="main_seaction9">
-            <img src="https://yuri-song.github.io/logo_image/mobile1/11.png" alt="">
+            <img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/11.png" alt="">
         </div>
         <div class="main_seaction10">
-            <img src="https://yuri-song.github.io/logo_image/mobile1/12.png" alt="">
+            <img src="https://cdn.jsdelivr.net/gh/yuri-song/logo_image/mobile1/12.png" alt="">
         </div>
     </div>
 </div>
