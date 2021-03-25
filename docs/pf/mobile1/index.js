a1100ed88d82461bbb28c1ca0a82e36aba39d43b
yurong51 <sy120003@gmail.com>
2021-03-26 01:10:39 +0900

    .

---
+++ docs/pf/mobile1/index.js
@@ -0,0 +1,66 @@
+console.clear;
+
+const markers = false;
+
+gsap.registerPlugin(ScrollTrigger);
+
+let t1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".main_seaction3",
+        start: "-=50 30%",
+        end: "bottom 85%",
+        markers: markers,
+        scrub: 0.3
+    }
+});
+
+t1.from('.main_seaction3 > img:nth-of-type(2)', {
+    y: -100,
+    opacity: 0,
+    duration: 0.5
+});
+
+t1.from('.main_seaction3 > img:nth-of-type(3)', {
+    y: -100,
+    opacity: 0,
+    duration: 0.5
+});
+
+t1.from('.main_seaction3 > img:nth-of-type(4)', {
+    y: -100,
+    opacity: 0,
+    duration: 0.5
+});
+
+
+let t2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".main_seaction4",
+        start: "-=50 30%",
+        end: "bottom 85%",
+        markers: markers,
+        scrub: 0.3
+    }
+});
+
+t2.from('.main_seaction4 > img:nth-of-type(2)', {
+    y: -100,
+    opacity: 0,
+    duration: 0.5
+});
+
+let t3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".main_seaction7",
+        start: "-=50 30%",
+        end: "bottom 85%",
+        markers: markers,
+        scrub: 0.3
+    }
+});
+
+t3.from('.main_seaction7 > img:nth-of-type(2)', {
+    y: -100,
+    opacity: 0,
+    duration: 0.5
+});
---
+++ docs/pf/web/odor/odor1.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
